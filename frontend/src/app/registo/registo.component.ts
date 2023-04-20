a64ca03b6f1cca03a37b5885d0cdd3d89f7a3d29
Afonso Santos <afonso.acc.santos@gmail.com>
2023-04-20 16:31:17 +0100

    html para registo

--- frontend/src/app/registo/registo.component.ts
+++ frontend/src/app/registo/registo.component.ts
@@ -8,7 +8,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegistoComponent {
   registrationForm = new FormGroup({
-    nomeDeUtilizador: new FormControl(''),
+    username: new FormControl(''),
     password: new FormControl(''),
   });
 }
