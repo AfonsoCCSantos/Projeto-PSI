93060cd187bed04aa5da7446cf7bc585367d3324
fc57099 <fc57099@alunos.fc.ul.pt>
2023-04-21 18:42:42 +0100

    Adicionado routing para paigna de login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginFormComponent} from "./login-form/login-form.component";
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
