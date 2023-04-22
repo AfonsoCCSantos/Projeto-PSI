6e3eff50f9d0a586c8aa6c6a3fc7bce8199727c5
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-04-22 15:46:36 +0100

    Implementação do item service
    inclui funções para error handling e error loging (Message Service)

--- frontend/src/app/item-searchbar/item-searchbar.component.ts
+++ frontend/src/app/item-searchbar/item-searchbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Item } from 'src/Item';
+import { ItemService } from '../item.service';
+
+import {
+    debounceTime, distinctUntilChanged, switchMap
+  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-item-searchbar',
   templateUrl: './item-searchbar.component.html',
   styleUrls: ['./item-searchbar.component.css']
 })
-export class ItemSearchbarComponent {
+export class ItemSearchbarComponent implements OnInit {
 
+    items$!: Observable<Item[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(private itemService : ItemService) {}
+
+    ngOnInit(): void {
+        this.items$ = this.searchTerms.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            switchMap((term: string) => this.itemService.searchHeroes(term)),
+        )
+    }
+
+    search(term: string): void {
+        this.searchTerms.next(term);
+    } 
 }
