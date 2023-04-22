1d2dd8fd614c91be20373dc6c190abefcb3fd774
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-04-22 15:48:52 +0100

    Revert "Implementação do item service"
    This reverts commit 6e3eff50f9d0a586c8aa6c6a3fc7bce8199727c5.

--- frontend/src/app/item-searchbar/item-searchbar.component.ts
+++ frontend/src/app/item-searchbar/item-searchbar.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { Item } from 'src/Item';
-import { ItemService } from '../item.service';
-
-import {
-    debounceTime, distinctUntilChanged, switchMap
-  } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-item-searchbar',
   templateUrl: './item-searchbar.component.html',
   styleUrls: ['./item-searchbar.component.css']
 })
-export class ItemSearchbarComponent implements OnInit {
+export class ItemSearchbarComponent {
 
-    items$!: Observable<Item[]>;
-    private searchTerms = new Subject<string>();
-
-    constructor(private itemService : ItemService) {}
-
-    ngOnInit(): void {
-        this.items$ = this.searchTerms.pipe(
-            debounceTime(300),
-            distinctUntilChanged(),
-            switchMap((term: string) => this.itemService.searchHeroes(term)),
-        )
-    }
-
-    search(term: string): void {
-        this.searchTerms.next(term);
-    } 
 }
--- frontend/src/app/item.service.ts
+++ frontend/src/app/item.service.ts
@@ -1,52 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
-
-import { Item } from 'src/Item';
-import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  private itemsUrl = '/items';
-
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
-  constructor(
-    private http : HttpClient,
-    private messageService : MessageService
-    ) { }
-
-  searchItems(term: string): Observable<Item[]> {
-    if(!term.trim()) {
-      return of([]);
-    }
-
-    return this.http.get<Item[]>(`${this.itemsUrl}/?name=${term}`).pipe(
-      catchError(this.handleError<Item []>('searchItems', []))
-      );
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-  
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-  
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-  
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  private log(message: string) {
-    this.messageService.add(`ItemService: ${message}`);
-  }
+  constructor() { }
 }
