08fce7260d30ab6326ad316afded68a322e024f5
diogo02pereira <diogo02pereira@gmail.com>
2023-04-23 10:21:51 +0100

    Adicionar user-profile component

---
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent {
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  user: User | undefined;
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const id = String(this.route.snapshot.paramMap.get('userName'));
+    this.userService.getHero(id)
+      .subscribe(user => this.user = user);
+  }
+
+}
