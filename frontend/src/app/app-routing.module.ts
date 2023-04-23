37c94ed679338a453a9431ff582ee1688cec2d1e
diogo02pereira <diogo02pereira@gmail.com>
2023-04-23 10:48:01 +0100

    Adicionar routing para o perfil

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 const routes: Routes = [
   {path: "", redirectTo: "/dashboard", pathMatch: "full" },
-  {path: "dashboard", component: DashboardComponent }
+  {path: "dashboard", component: DashboardComponent },
+  {path: "profile", component: UserProfileComponent},
 ];
 
 @NgModule({
