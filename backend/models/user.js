7a32b7cee643e20522b8df16609e001662bad2fc
diogo02pereira <diogo02pereira@gmail.com>
2023-04-23 10:10:19 +0100

    Adicionar UserSchema

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  userName: { type: String , require : true, unique : true},
+  image: { type: Buffer, required: true}, 
+
+});
+
+module.exports = mongoose.model("User", UserSchema);
