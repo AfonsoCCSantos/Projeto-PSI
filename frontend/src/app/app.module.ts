71e1e288055b8c4296c9dcaa9482c2542ef1f35b
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-04-23 06:39:26 +0100

    Adicionar HttpClientModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemSearchbarComponent } from './item-searchbar/item-searchbar.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { ItemSearchbarComponent } from './item-searchbar/item-searchbar.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
