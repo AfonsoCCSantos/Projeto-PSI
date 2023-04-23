b46e6f1027f07df5c3db00c2743f84efb9a9dd71
Afonso Santos <afonso.acc.santos@gmail.com>
2023-04-23 19:46:06 +0100

    fixed conflit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistoComponent } from './registo/registo.component';
 
-<<<<<<< HEAD
 import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   {path: "", redirectTo: "/dashboard", pathMatch: "full" },
-  {path: "dashboard", component: DashboardComponent }
-=======
-const routes: Routes = [
-  { path: 'register', component: RegistoComponent },
->>>>>>> PSI1-28
+  {path: "dashboard", component: DashboardComponent },
+  {path: 'register', component: RegistoComponent }
 ];
 
 @NgModule({
