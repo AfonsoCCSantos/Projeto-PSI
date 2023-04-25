7586e2be9cc073fcf4a6396f31718bccf6032ed6
diogo02pereira <diogo02pereira@gmail.com>
2023-04-25 10:28:14 +0100

    Adicionar nome ao ItemSchema

--- backend/models/item.js
+++ backend/models/item.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    type : { type : String, require: true},
+    type : { type : String, require : true},
+    name : { type : String, require : true},
     description : { type : String, require : true, maxLength : 1000},
     platform : { type : String, require : true},
     languages : { type : [String], require : true},
