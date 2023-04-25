f72d8aea05b807e437121316d432e44dce470310
diogo02pereira <diogo02pereira@gmail.com>
2023-04-25 10:18:57 +0100

    Updating UserSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   userName: { type: String , require : true, unique : true},
-  image: { type: String, required: true},
+  password: { type: String, require : true},
+  image: { type: Buffer, required: true},
+  item_library: { type: [Schema.Types.ObjectId, ref = "Item" ]},
+  wish_items: { type: [Schema.Types.ObjectId, ref = "Item" ]},
+  followers_list: { type: [Schema.Types.ObjectId, ref = "User" ]},
+  following_list: { type: [Schema.Types.ObjectId, ref = "User" ]}
 
 });
 
