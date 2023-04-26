e4e4db4b9521a13761eee09e244ef7c07a7a31f8
Afonso Santos <afonso.acc.santos@gmail.com>
2023-04-26 12:40:26 +0100

    Added register user function to user.service

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
 import { User } from './user';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,26 @@ import { User } from './user';
 export class UserService {
 
   private baseUrl = 'http://localhost:3000';
-  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
+  constructor(private http: HttpClient) { }
 
   getUserByName(username: string): Observable<User> {
     const url = `${ this.baseUrl + "/user"}/${username}`;
     return this.http.get<User>(url);
+  }
+
+  registerUser(user: User): Observable<boolean> {
+    const url = `${ this.baseUrl + "/user/register"}`;
+    return this.http.post<HttpResponse<any>>(url, user, this.httpOptions).pipe(
+      map((response : HttpResponse<any>) => {
+        return response.status === 200;
+      }),
+      catchError(() => {
+        return of(false);
+      })
+    );
   }
 }
