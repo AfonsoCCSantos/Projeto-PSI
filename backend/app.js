d0f869282fbf60f95bfafb6c989f1a1f26408098
fc57099 <fc57099@alunos.fc.ul.pt>
2023-04-26 12:52:47 +0100

    Intalling mongoose

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://alex:test123@cluster0.llpr2dj.mongodb.net/PSI?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
