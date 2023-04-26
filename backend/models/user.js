81e3bdfc8e6c82215ddb72d4766cd7e112f2f7ca
fc57099 <fc57099@alunos.fc.ul.pt>
2023-04-26 12:30:51 +0100

    Changed User field name

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  userName: { type: String , require : true, unique : true},
+  name: { type: String , require : true, unique : true},
   password: { type: String, require : true},
   image: { type: Buffer, required: true},
   item_library: { type: [Schema.Types.ObjectId, ref = "Item" ]},
