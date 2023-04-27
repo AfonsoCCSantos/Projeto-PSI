9de6e0a5760b135fe620fab84aad978933466119
fc57099 <fc57099@alunos.fc.ul.pt>
2023-04-27 21:59:38 +0100

    Changed proxy url for backend

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user', usersRouter);
+app.use('/api/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
