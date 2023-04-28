d0f73fda427de463edea22a1a642c45b343839af
TOXIK523 <tomasalexandrebarreto@gmail.com>
2023-04-28 18:31:51 +0100

    Alterar o url do endpoint do search no Items.js

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const item_controller = require("../controllers/itemController");
 
 /*GET items by term in search bar*/
-router.get('/items/?name=:term', item_controller.items_search_list);
+router.get('/search/?name=:term', item_controller.items_search_list);
 
 router.post('/', item_controller.add_item);
 
