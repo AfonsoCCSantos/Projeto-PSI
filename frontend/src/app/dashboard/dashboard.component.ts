646e72076545a77d421686b289d8448f6afa9572
Afonso Santos <afonso.acc.santos@gmail.com>
2023-05-03 12:15:31 +0100

    adicionado titulo a dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 import { ItemService } from "../item.service";
 import {Item} from "../Item";
@@ -9,15 +10,15 @@ import {Item} from "../Item";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  title = 'Cetime';
 
-  constructor(private itemService : ItemService) {}
+  constructor(private itemService : ItemService, private titleService:Title) {
+    this.titleService.setTitle("Welcome to Cetime");
+  }
 
   games : Item[] | undefined;
-
+  
   ngOnInit() {
     this.itemService.getItems().subscribe(items => this.games = items);
   }
-
-
-
 }
