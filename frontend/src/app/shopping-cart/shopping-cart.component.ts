9c0518d2a883e1c575cff6b463b265d7c12fbca2
Afonso Santos <afonso.acc.santos@gmail.com>
2023-05-06 17:43:31 +0100

    added functions to insert, increase and decrease item quantity in the shopping cart

--- frontend/src/app/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/shopping-cart/shopping-cart.component.ts
@@ -19,4 +19,38 @@ export class ShoppingCartComponent {
   goToShoppingCartView() {
     this.router.navigate(["shoppingcart"]);
   }
+
+  addItemToShoppingCart(itemId: string) {
+    let currentItemsInShoppingCart = this.getItemsInShoppingCart();
+    if (!currentItemsInShoppingCart) { //There were no items in the shopping cart
+      currentItemsInShoppingCart = {[itemId]: 1}
+    }
+    else { //There were already items in the shopping cart
+      currentItemsInShoppingCart[itemId] = 1;
+    }
+    localStorage.setItem('shoppingCartItems', JSON.stringify(currentItemsInShoppingCart));
+  }
+  
+  increaseQuantityOfItemInShoppingCart(itemId: string) {
+    this.updateQuantityOfItemInShoppingCart(itemId, 1);
+  }
+
+  decreaseQuantityOfItemInShoppingCart(itemId: string) {
+    this.updateQuantityOfItemInShoppingCart(itemId, -1);
+  }
+
+  private getItemsInShoppingCart(): any {
+    let currentItemsInShoppingCart = null;
+    const savedShoppingCartItems = localStorage.getItem('shoppingCartItems');
+    if (savedShoppingCartItems != null) {
+      currentItemsInShoppingCart = JSON.parse(savedShoppingCartItems);
+    }
+    return currentItemsInShoppingCart;
+  }
+
+  private updateQuantityOfItemInShoppingCart(itemId: string, quantityToUpdate: number) {
+    let currentItemsInShoppingCart = this.getItemsInShoppingCart();
+    currentItemsInShoppingCart[itemId] = currentItemsInShoppingCart[itemId] + quantityToUpdate;
+    localStorage.setItem('shoppingCartItems', JSON.stringify(currentItemsInShoppingCart));
+  }
 }
