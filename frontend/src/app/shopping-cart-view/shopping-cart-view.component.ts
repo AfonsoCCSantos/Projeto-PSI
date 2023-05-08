4a8e4b6ede61b051161f3b0f4483dcff28308d49
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2023-05-08 20:01:40 +0100

    adicionado total do carrinho

--- frontend/src/app/shopping-cart-view/shopping-cart-view.component.ts
+++ frontend/src/app/shopping-cart-view/shopping-cart-view.component.ts
@@ -10,22 +10,26 @@ import {ShoppingCartService} from "../shopping-cart.service";
 export class ShoppingCartViewComponent {
 
   games = new Map();
+  totalPrice = 0;
 
   constructor(private itemService : ItemService, private shoppingCartService : ShoppingCartService) {}
 
   ngOnInit() {
-    // /*So para testar*/
+    /*So para testar*/
     // this.shoppingCartService.addItemToShoppingCart("644bf9d023ef3a462196c92d");
     // this.shoppingCartService.addItemToShoppingCart("644c01db032b9210a50a566c");
     // this.shoppingCartService.addItemToShoppingCart("64553a140e9cb42cbda729d9");
-    // /*              */
+    /*              */
 
     let items = this.shoppingCartService.getItemsInShoppingCart();
     let mapGames = new Map<string,Number>(Object.entries(items));
 
 
     for(let item of mapGames.keys()) {
-      this.itemService.getItem(item).subscribe(i =>  {this.games.set(i, mapGames.get(item)); });
+      this.itemService.getItem(item).subscribe(currItem =>  {
+        this.games.set(currItem, mapGames.get(item));
+        this.totalPrice += Number(currItem.price);
+      });
 
     }
       console.log(this.games.keys());
