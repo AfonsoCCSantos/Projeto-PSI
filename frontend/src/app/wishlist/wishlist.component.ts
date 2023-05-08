afb8eb05b06b66159a9541ca7ac773d397820a16
diogo02pereira <diogo02pereira@gmail.com>
2023-05-08 20:00:23 +0100

    Adicionar lista de itens na wishlist
    Ao aceder à wishlist, vai buscar todos os items da wishlist do user

--- frontend/src/app/wishlist/wishlist.component.ts
+++ frontend/src/app/wishlist/wishlist.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../user.service";
+import {User} from "../user";
+import {Item} from "../Item";
+import {ItemService} from "../item.service";
+
+
 
 @Component({
   selector: 'app-wishlist',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./wishlist.component.css']
 })
 export class WishlistComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private itemService: ItemService
+  ) {}
 
+  user: User | undefined;
+  items: [Item] | undefined
+
+
+  ngOnInit(): void {
+    this.getUser();
+    this.getItems();
+  }
+
+  getUser(): void {
+    const user_name = String(this.route.snapshot.paramMap.get('userName'));
+    this.userService.getUserByName(user_name)
+      .subscribe(user => this.user = user);
+  }
+
+
+  private getItems() {
+    // @ts-ignore
+    for (let i = 0; i < this.user?.wish_items?.length; i++) {
+      // @ts-ignore
+      let item = this.itemService.getItem(this.user?.wish_items[i]);
+      // @ts-ignore
+      this.items?.push(item)
+    }
+  }
 }
