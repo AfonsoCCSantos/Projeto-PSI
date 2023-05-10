e3844e1d4e79b74172f914b301a533d898828490
diogo02pereira <diogo02pereira@gmail.com>
2023-05-10 12:15:33 +0100

    Display finalizado wishlist

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,10 +17,10 @@ import { LibraryComponent } from './library/library.component';
 import { ListsComponent } from './lists/lists.component';
 import { FollowersComponent } from './followers/followers.component';
 import { FollowingComponent } from './following/following.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
 import { AddToWishlistButtonComponent } from './add-to-wishlist-button/add-to-wishlist-button.component';
 import {ShoppingCartViewComponent} from "./shopping-cart-view/shopping-cart-view.component";
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import {WishlistComponent} from "./wishlist/wishlist.component";
 
 
 @NgModule({
